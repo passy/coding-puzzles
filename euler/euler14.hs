3321f2097848af72888249b548d6d69f05700dad
Pascal Hartig <phartig@rdrei.net>
2012-06-03 19:59:29 +0200

    Added simple parallelization for euler14

--- euler/euler14.hs
+++ euler/euler14.hs
@@ -1,4 +1,5 @@
 import Data.List
+import Control.Parallel
 
 -- Collatz Problem
 
@@ -17,14 +18,23 @@ collatzlen :: (Integral a) => a -> Int
 collatzlen = (+ 1) . length . takeWhile (/=1) . collatz
 
 
+collatztupel :: (Integral a) => a -> [(Int, a)]
+collatztupel num =
+    par a1 $ seq a2 (a1 ++ a2)
+    where
+        a1 = [(collatzlen n, n) | n <- [2..(num `div` 2)]]
+        a2 = [(collatzlen n, n) | n <- [(num `div` 2 + 1)..((num - 1))]]
+
+
 -- With array caching, this would already be much faster!
 -- Also, one could make use of parallel processing here easily.
 euler14 =
-    let list = [(collatzlen n, n) | n <- [2..(10^6 - 1)]]
+    let
+        list = collatztupel (10^6)
         sortGT (a1, b1) (a2, b2)
-            | a1 < a2 = GT
-            | a1 > a2 = LT
-            | otherwise = EQ
+                | a1 < a2 = GT
+                | a1 > a2 = LT
+                | otherwise = EQ
     in
         head $ sortBy sortGT list
 
