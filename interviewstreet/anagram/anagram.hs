63e34a7e27859e96004d941e8002dfa857c086cc
Pascal Hartig <phartig@rdrei.net>
2013-07-06 22:02:22 +0200

    Added anagram hackerrank solution in Haskell

---
+++ interviewstreet/anagram/anagram.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+import Control.Monad (forM_)
+import Data.List (nub, elemIndex)
+
+
+calcAnagram :: String -> Int
+calcAnagram str =
+    let
+        l = length str
+        a = take (l `div` 2) str
+        b = nub $ drop (l `div` 2) str
+        loop (x:xs) bl c =
+            case elemIndex x b of
+                Just i -> loop xs (take i bl ++ drop (i + 1) bl) c
+                Nothing -> loop xs bl (c + 1)
+        loop [] _ c = c
+    in
+        loop a b 0
+
+run :: IO ()
+run = do
+    line <- getLine
+    putStrLn $ if length line `mod` 2 == 1
+        then "-1"
+        else show $ calcAnagram line
+
+
+main :: IO ()
+main = do
+    (n :: Integer) <- read `fmap` getLine
+    forM_ [0..n-1] $ const run
