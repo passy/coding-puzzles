750aa9c515bb6378a4be6b717428457fe89200ad
Pascal Hartig <phartig@rdrei.net>
2013-07-10 00:42:15 +0200

    Corrected flowers implementation

--- interviewstreet/flowers/flowers.hs
+++ interviewstreet/flowers/flowers.hs
@@ -5,25 +5,28 @@ import Data.List (sort)
 mapTuple ::  Arrow a => a b' c' -> a (b', b') (c', c')
 mapTuple f = f *** f
 
-calcM :: Int -> Int -> [Int]
+calcM :: Int -> Int -> [Integer]
 calcM n k = take n $ concat [replicate k x | x <- [1..]]
 
-minimumPrice :: Int -> Int -> [Int] -> Int
+minimumPrice :: Int -> Int -> [Integer] -> Integer
 minimumPrice n k flowers =
     let
         sorted = sort flowers
-        m = calcM n k
+        m = reverse $ calcM n k
     in
         sum [x * y | (x, y) <- zip sorted m]
 
-run :: Int -> Int -> [Int] -> IO ()
+run :: Int -> Int -> [Integer] -> IO ()
 run n k flowers = print $ minimumPrice n k flowers
 
 readi :: String -> Int
 readi = read
 
+readi' :: String -> Integer
+readi' = read
+
 main :: IO ()
 main = do
     (n, k) <- fmap (mapTuple readi . break (== ' ')) getLine
-    flowers <- fmap (map readi . words) getLine
+    flowers <- fmap (map readi' . words) getLine
     run n k flowers
