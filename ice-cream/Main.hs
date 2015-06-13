36cfc8724ba1750a76e8c8dc4e32cc82e574f2bd
Pascal Hartig <phartig@twitter.com>
2015-06-13 17:00:52 +0100

    Fixed slicing, formatting

--- ice-cream/Main.hs
+++ ice-cream/Main.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
 
 import Data.Traversable (forM)
+import Data.Foldable (forM_)
 import Data.List (sortOn)
 import Data.Maybe (fromMaybe)
 import qualified Data.Vector as V
@@ -11,17 +12,16 @@ run = do
     (n :: Int)           <- read <$> getLine
     (xs :: V.Vector (Int, Int)) <- V.fromList <$> reverse <$> sortOn snd <$> zip [1..] <$> (fmap read . words) <$> getLine
 
-    let r = fmap (f m xs) xs
+    let r = V.imap (f m xs) xs
 
-    print r
     return . sortTuple . V.head . V.filter ((/= 0) . snd) $ r
 
  where
-    f :: Int -> V.Vector (Int, Int) -> (Int, Int) -> (Int, Int)
-    f m xs x =
+    f :: Int -> V.Vector (Int, Int) -> Int -> (Int, Int) -> (Int, Int)
+    f m xs i x =
         let a = (m - snd x)
-            -- rest = V.slice (fst x - 1) (length x - fst x) xs
-            maybeMatch = fmap fst $ V.find ((== a) . snd) xs
+            rest = V.slice (i + 1) (length xs - i - 1) xs
+            maybeMatch = fmap fst $ V.find ((== a) . snd) rest
         in (fst x, fromMaybe 0 maybeMatch)
 
     sortTuple :: (Int, Int) -> (Int, Int)
@@ -33,4 +33,4 @@ main :: IO ()
 main = do
     (t :: Int) <- read <$> getLine
     res <- forM [0..(t - 1)] $ const run
-    print res
+    forM_ res $ \(a, b) -> putStrLn $ unwords [show a, show b]
