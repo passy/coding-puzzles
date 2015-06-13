650e37ace761fe0acf0d228eefd8789dace7af61
Pascal Hartig <phartig@twitter.com>
2015-06-13 16:48:49 +0100

    Almost.
    Need to figure out how to do the slicing right, but I think the logic is
    sound otherwise.

--- ice-cream/Main.hs
+++ ice-cream/Main.hs
@@ -1,4 +1,36 @@
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
+
+import Data.Traversable (forM)
+import Data.List (sortOn)
+import Data.Maybe (fromMaybe)
+import qualified Data.Vector as V
+
+run :: IO (Int, Int)
+run = do
+    (m :: Int)           <- read <$> getLine
+    (n :: Int)           <- read <$> getLine
+    (xs :: V.Vector (Int, Int)) <- V.fromList <$> reverse <$> sortOn snd <$> zip [1..] <$> (fmap read . words) <$> getLine
+
+    let r = fmap (f m xs) xs
+
+    print r
+    return . sortTuple . V.head . V.filter ((/= 0) . snd) $ r
+
+ where
+    f :: Int -> V.Vector (Int, Int) -> (Int, Int) -> (Int, Int)
+    f m xs x =
+        let a = (m - snd x)
+            -- rest = V.slice (fst x - 1) (length x - fst x) xs
+            maybeMatch = fmap fst $ V.find ((== a) . snd) xs
+        in (fst x, fromMaybe 0 maybeMatch)
+
+    sortTuple :: (Int, Int) -> (Int, Int)
+    sortTuple (a, b)
+        | a > b     = (b, a)
+        | otherwise = (a, b)
+
 main :: IO ()
 main = do
-    putStrLn "yo"
-    return ()
+    (t :: Int) <- read <$> getLine
+    res <- forM [0..(t - 1)] $ const run
+    print res
