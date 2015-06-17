9b532be340dba040874dbc803d6bc10545c86a0e
Pascal Hartig <phartig@twitter.com>
2015-06-17 20:53:01 +0100

    Ugly, but functional (heh) solution

--- hackerrank/diagonal-difference/Main.hs
+++ hackerrank/diagonal-difference/Main.hs
@@ -9,4 +9,12 @@ main = do
     lines <- replicateM n getLine :: IO [String]
     let res = fmap read . words <$> lines :: [[Int]]
 
-    forM_ res print
+    -- Surely, there's a way nicer approach just folding over this, right?
+    let lrindices = [ (m, m) | m <- [0..(n - 1)] ]
+    let rlindices = [ (m, n - 1 - m) | m <- [0..(n - 1)] ]
+
+    print $ abs $ (isum lrindices res) - (isum rlindices res)
+
+  where
+    isum indices lines =
+        sum $ (\(x, y) -> lines !! x !! y) <$> indices
