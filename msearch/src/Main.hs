e74e08c6775b300a7242fc7c3a98bff3d7577584
Pascal Hartig <phartig@rdrei.net>
2015-10-10 22:40:33 +0100

    Eeek, what a mess. Let's do this right tomorrow

--- msearch/src/Main.hs
+++ msearch/src/Main.hs
@@ -19,8 +19,35 @@ findEntryIndices matrix (firstChar:_) =
   foldl (\acc (i, cols) ->
     ((\(j, _) -> (i, j)) <$> filter ((firstChar ==) . snd) cols) <> acc) [] matrix
 
-findWord :: CMatrix -> String -> (Int, Int) -> [(Int, Int)]
-findWord = undefined
+findWord :: CMatrix -> String -> (Int, Int) -> [(Int, Int, Char)] -> [(Int, Int, Char)]
+findWord []     _    _     _    = []
+findWord matrix word start path =
+  if (thd3 <$> path) == word then path
+  else run matrix word start path
+
+  where
+    run matrix (w:ws) (x, y) path = do
+      let matrix' = reducedMatrix x y
+      -- TODO: Check bounds.
+      if snd (matrix !! x !! y) == w
+        then
+          let path' = (x, y, w) : path
+          in
+            findWord matrix' ws (x - 1, y) path' <>
+            findWord matrix' ws (x + 1, y) path' <>
+            findWord matrix' ws (x, y - 1) path' <>
+            findWord matrix' ws (x, y + 1) path'
+      else
+        findWord matrix' ws (x - 1, y) path <>
+        findWord matrix' ws (x + 1, y) path <>
+        findWord matrix' ws (x, y - 1) path <>
+        findWord matrix' ws (x, y + 1) path
+
+    reducedMatrix :: Int -> Int -> CMatrix
+    reducedMatrix _ _ = matrix -- Should be the matrix without those coords
+
+thd3 :: (a, b, c) -> c
+thd3 (_, _, c) = c
 
 indexMatrix :: String -> CMatrix
 indexMatrix s =
