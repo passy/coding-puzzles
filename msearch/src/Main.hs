d1a834e68b7f8a6b40f9e0f33bebcdb1d4ce27f2
Pascal Hartig <phartig@rdrei.net>
2015-10-10 14:28:53 +0100

    Find all entry points

--- msearch/src/Main.hs
+++ msearch/src/Main.hs
@@ -1,14 +1,25 @@
 module Main where
 
+import Data.Monoid ((<>))
 import System.Environment (getArgs)
+
+type CMatrix = [(Int, [(Int, Char)])]
 
 main :: IO ()
 main = do
   (fname:_) <- getArgs
-  contents <- indexMatrix <$> readFile fname
-  print $ contents
+  mat <- indexMatrix <$> readFile fname
+  let starts = findEntryIndices mat "LONDON"
+  print starts
 
-indexMatrix :: String -> [(Int, [(Int, Char)])]
+
+findEntryIndices :: CMatrix -> String -> [(Int, Int)]
+findEntryIndices matrix word = do
+  let (firstChar:_) = word
+  foldl (\acc (i, cols) ->
+    ((\(j, _) -> (i, j)) <$> filter (\(j, c) -> c == firstChar) cols) <> acc) [] matrix
+
+indexMatrix :: String -> CMatrix
 indexMatrix s =
     let ls = lines s
         enumerate = zip [0..]
