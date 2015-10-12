5e61f0837ab9f4c4309c5892f9638d320a023861
Pascal Hartig <phartig@rdrei.net>
2015-10-12 11:45:05 +0100

    Use a real set

--- msearch/src/Main.hs
+++ msearch/src/Main.hs
@@ -3,6 +3,7 @@ module Main where
 
 import Data.Monoid ((<>))
 import System.Environment (getArgs)
+import qualified Data.Set as Set
 
 type CMatrix = [(Int, [(Int, Char)])]
 type Step = (Int, Int, Char)
@@ -19,7 +20,7 @@ main = do
   -- looking for.
   let starts = findEntryIndices mat word
   -- All possibly partial paths from the given start points to the word.
-  let paths = findWord mat word [] <$> starts
+  let paths = findWord mat word mempty <$> starts
   -- Only paths that contain the entire word.
   let fullPaths = filter (isFullPath word) paths
   print fullPaths
@@ -36,16 +37,16 @@ findEntryIndices matrix (firstChar:_) =
   foldl (\acc (i, cols) ->
     ((\(j, _) -> (i, j)) <$> filter ((firstChar ==) . snd) cols) <> acc) [] matrix
 
-findWord :: CMatrix -> String -> [(Int, Int)] -> (Int, Int) -> Node Step
+findWord :: CMatrix -> String -> Set.Set (Int, Int) -> (Int, Int) -> Node Step
 findWord matrix (w:w':ws) visited (x, y) =
   let directions = [ (x + 1, y)
                    , (x, y + 1)
                    , (x - 1, y)
                    , (x, y - 1)
                    ]
-      -- The "Set" (TODO: Make this an actual set) of coordinates we've looked
-      -- at before plus the one we're looking at right now.
-      visited' = (x, y) : visited
+      -- The Set of coordinates we've looked  at before plus the one
+      -- we're looking at right now.
+      visited' = Set.insert (x, y) visited
       -- Coordinates plus value of all neighbors
       neighbors = (\(x', y') -> (x', y', lookup x' matrix >>= lookup y')) <$> directions
       -- Only neighbors the contain the value we look at AND are within
@@ -54,7 +55,7 @@ findWord matrix (w:w':ws) visited (x, y) =
       -- The coordinates of all valid neighbors
       coords = (\(x', y', _) -> (x', y')) <$> validNeighbors
       -- The coordinates of all valid neighbors that we haven't visited yet.
-      nonVisitedCoords = filter (`notElem` visited) coords
+      nonVisitedCoords = filter (`Set.notMember` visited) coords
       -- Careful: Not tail-recursive!
       children = findWord matrix (w':ws) visited' <$> nonVisitedCoords
   in
