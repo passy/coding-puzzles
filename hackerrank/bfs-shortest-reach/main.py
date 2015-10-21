ddb15426774fd434571982149416a1aca105c2df
Pascal Hartig <phartig@rdrei.net>
2015-10-21 21:37:39 +0100

    I'm gonna vom

--- hackerrank/bfs-shortest-reach/main.py
+++ hackerrank/bfs-shortest-reach/main.py
@@ -19,16 +19,37 @@ def read_tuple():
 def run():
     n, m = read_tuple()
     connections = [read_tuple() for _ in range(m)]
-    start = int(input()) - 1
+    start = int(input())
 
     # Adjacency matrix
-    matrix = collections.defaultdict(lambda: [])
+    matrix = collections.defaultdict(lambda: set())
     for a, b in connections:
-        matrix[a].append(b)
-        matrix[b].append(a)
+        matrix[a].add(b)
+        matrix[b].add(a)
 
-    print("Matrix: ", matrix)
-    print("Start: ", start)
+    queue = [start]
+    cost = {start: 0}
+    cur_cost = COST
+
+    # I love how disgustingly imperative this is.
+    while queue:
+        i = queue.pop(0)
+        for _n in neighbors(matrix, i):
+            if _n not in cost:
+                cost[_n] = cur_cost
+                queue.append(_n)
+        cur_cost += COST
+
+    for i in range(2, n + 1):
+        if i not in cost:
+            print(-1, end=' ')
+        else:
+            print(cost[i], end=' ')
+
+    print()
+
+def neighbors(matrix, i):
+    return matrix[i]
 
 
 if __name__ == "__main__":
