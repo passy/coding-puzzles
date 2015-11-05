bb197271c930626279582ac5da871e59a6a5515b
Pascal Hartig <phartig@rdrei.net>
2015-11-05 12:43:12 +0000

    Remove the lambda

--- staircase/Main.hs
+++ staircase/Main.hs
@@ -3,7 +3,7 @@ import Control.Monad (forM_, join)
 main :: IO ()
 main = do
   n <- readLn :: IO Int
-  let stairs = (\(m, n) -> stair m n) <$> [(m, n) | m <- [1..n]]
+  let stairs = uncurry stair <$> [(m, n) | m <- [1..n]]
   forM_ stairs putStrLn
 
 stair :: Int -> Int -> String
