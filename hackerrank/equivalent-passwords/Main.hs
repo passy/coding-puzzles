6b5b4ec05a47993a5ed0f1121a09a8807d97ec2c
Pascal Hartig <phartig@rdrei.net>
2015-11-15 12:59:41 +0000

    Desperate atttempt

--- hackerrank/equivalent-passwords/Main.hs
+++ hackerrank/equivalent-passwords/Main.hs
@@ -3,9 +3,10 @@ import           Control.Arrow
 import           Control.Monad
 import           Data.Char           (digitToInt)
 import           Data.List
-import           Data.Function (on)
+import           Data.Function       (on)
 import           Debug.Trace
 import qualified Data.Map.Strict     as Map
+import qualified Data.Set            as Set
 
 wschars :: String
 wschars = " \t\r\n"
@@ -47,15 +48,14 @@ runTest i = do
   t <- readLn :: IO Int
   ls <- fmap strip <$> replicateM t getLine
   -- let m = foldl' (\b a -> Map.insertWith (++) (length a) a b) Map.empty ls
-  let res = go Map.empty ls 0
+  let res = go ls
   putStrLn $ "Case " ++ show i ++ ": " ++ show res
 
-go :: Map.Map Int [String] -> [String] -> Int -> Int
-go _ []     acc = acc
-go m (x:xs) acc =
-  let m' = Map.insertWith (++) (length x) [x] m
-      res = maybe False (any (equivalent x)) (Map.lookup (length x) m)
-  in if res then go m xs acc else go m' xs (acc + 1)
+go :: [String] -> Int
+go is =
+  let iis = read <$> is
+      s' = Set.fromList $ normalize <$> iis
+  in Set.size s'
 
 -- Careful, I dropped the length check here since that invariant is controlled
 -- higher up.
