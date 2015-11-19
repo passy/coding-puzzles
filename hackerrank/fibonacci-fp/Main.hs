32780efe1972f2f1ccf05f1cdd4351092129825f
Pascal Hartig <phartig@rdrei.net>
2015-11-19 22:48:19 +0100

    Expected something cooler for DP, tbh

--- hackerrank/fibonacci-fp/Main.hs
+++ hackerrank/fibonacci-fp/Main.hs
@@ -2,17 +2,18 @@ module Main where
 
 import Control.Monad
 
-fib :: Integer -> Integer
-fib 0 = 0
-fib 1 = 1
-fib n = fib (n - 1) + fib (n - 2)
+fib :: Int -> Integer
+fib = (map go [0..] !!)
+  where go 0 = 0
+        go 1 = 1
+        go n = fib (n - 1) + fib (n - 2) `rem` (10 ^ 8 + 7)
 
-weirdFib :: Integer -> Integer
+weirdFib :: Int -> Integer
 weirdFib n = fib n `rem` (10 ^ 8 + 7)
 
 main :: IO ()
 main = do
   t <- readLn :: IO Int
   replicateM_ t $ do
-    n <- readLn :: IO Integer
+    n <- readLn :: IO Int
     print $ weirdFib n
