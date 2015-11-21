b6ee1f7cd0c264ff3baec3053ec676c53f92186a
Pascal Hartig <phartig@rdrei.net>
2015-11-21 23:08:01 +0000

    Update euler16.hs

--- euler/euler16.hs
+++ euler/euler16.hs
@@ -2,4 +2,4 @@ import Data.Char
 
 -- What? Someone was lazy coming up with new problems, I guess. ;)
 main = do
-    putStrLn . show $ sum $ map digitToInt $ show $ 2^1000
+    putStrLn . show . sum . map digitToInt . show $ 2^1000
