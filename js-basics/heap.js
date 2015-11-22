c73e07a772b227a22b62bdc7e8117a9fc264f0c6
Pascal Hartig <phartig@rdrei.net>
2015-11-22 15:36:55 +0100

    Poor person's heap sort

--- js-basics/heap.js
+++ js-basics/heap.js
@@ -20,7 +20,7 @@ BinaryHeap.prototype = {
     let j = i;
 
     while (j > 0) {
-      const parentI = Math.floor(((i + 1) / 2) - 1);
+      const parentI = Math.floor((i - 1) / 2);
       const parent = this.content[parentI];
       const parentScore = this.cmp(parent);
 
@@ -30,6 +30,49 @@ BinaryHeap.prototype = {
       this.content[parentI] = el;
       j = parentI;
     }
+  },
+
+  siftDown(i) {
+    const el = this.content[i], score = this.cmp(el);
+    let j = i;
+
+    const swap = (i, j) => {
+      const tmp = this.content[i];
+      this.content[i] = this.content[j];
+      this.content[j] = tmp;
+    }
+
+    while (true) {
+      console.log('content: ', this.content);
+      const childi = j * 2;
+      const a = this.content[childi + 1];
+      const b = this.content[childi + 2];
+      const scoreA = this.cmp(a);
+      const scoreB = this.cmp(b);
+
+      if (a !== undefined && score > scoreA && scoreA < scoreB) {
+        console.log('swap(', j, childi + 1, ')');
+        swap(j, childi + 1);
+        j = a;
+      } else if (b !== undefined && score > scoreB && scoreB < scoreA) {
+        console.log('swap(', j, childi + 2, ')');
+        swap(j, childi + 2);
+        j = b;
+      } else {
+        break;
+      }
+    }
+  },
+
+  pop() {
+    const e = this.content[0];
+    this.content[0] = this.content.pop();
+    this.siftDown(0);
+    return e;
+  },
+
+  isEmpty() {
+    return this.content.length === 0;
   }
 };
 
@@ -41,8 +84,21 @@ const main = () => {
   bh.push(10);
   bh.push(500);
 
-  console.log(bh.peek());
-  console.log(bh.content);
+  const bh2 = new BinaryHeap(id);
+  // Should pull in some property testing libs
+  for (let i = 0; i < 500; i++) {
+    // Yeah, really don't care about precision here.
+    bh2.push(Math.floor(Math.random() * 100));
+    console.log(bh2.content);
+  }
+
+  const res = [];
+  while (!bh2.isEmpty()) {
+    res.push(bh2.pop());
+    console.log(res);
+  }
+
+  console.log(res);
 };
 
 main();
