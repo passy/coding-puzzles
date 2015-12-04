5c9188b9a7ad005648963f0b859e298fb5b2ec36
Pascal Hartig <phartig@rdrei.net>
2015-12-04 20:43:58 +0000

    More compact code

--- hackerrank/string-o-permute/Main.hs
+++ hackerrank/string-o-permute/Main.hs
@@ -1,16 +1,13 @@
 module Main where
 
-import Control.Monad
+import           Control.Monad
 
 perms :: String -> String
-perms s = go s []
-  where
-    go [] acc = reverse acc
-    go (x:y:xs) acc = go xs (x : y : acc)
-    go [x] acc = go [] (x : acc)
+perms []       = []
+perms (x:y:xs) = y : x : perms xs
+perms [x]      = x : perms []
 
 main :: IO ()
 main = do
   t <- readLn :: IO Int
-  replicateM_ t $
-    perms <$> getLine >>= putStrLn
+  replicateM_ t $ perms <$> getLine >>= putStrLn
