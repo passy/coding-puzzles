7ccb18dde5101d7e2d6ff9de07b64f7d9bfeb626
Pascal Hartig <phartig@rdrei.net>
2015-12-06 15:48:24 +0000

    Maybes are Alternatives! Thanks 24 Pull Requests!

--- hackerrank/connected-cell-in-a-grid/Main.hs
+++ hackerrank/connected-cell-in-a-grid/Main.hs
@@ -1,7 +1,9 @@
 module Main where
 
 import Control.Applicative
+import Data.List
 import Control.Monad
+import Debug.Trace
 
 data Filled = F | E
   deriving (Show, Eq)
@@ -21,10 +23,30 @@ children (x, y) = [ ( x, y + 1 )
                   , ( x - 1, y - 1 )
                   ]
 
+(!!!) :: [a] -> Int -> Maybe a
+(!!!) arr idx
+  | idx < 0 = Nothing
+  | idx >= length arr = Nothing
+  | otherwise = Just (arr !! idx)
+
+compute :: [[Filled]] -> Int
+compute matrix =
+  let start = traceShowId $ findNext matrix (0, 0)
+  in start `seq` 0
+
+findNext :: [[Filled]] -> (Int, Int) -> Maybe (Int, Int)
+findNext matrix start@(x, y) = do
+  row <- matrix !!! x
+  cell <- row !!! y
+  if cell == F then
+    return start
+  else
+    findNext matrix (x, y + 1) <|> findNext matrix (x + 1, 0)
+
 main :: IO ()
 main = do
   n <- readLn :: IO Int
   m <- readLn :: IO Int
 
   matrix <- replicateM m $ take n <$> fmap readFilled <$> words <$> getLine
-  print matrix
+  print $ compute matrix
