f29fbc471e524a542490b6d8f628a4ead4e8be93
Pascal Hartig <phartig@rdrei.net>
2015-12-26 10:39:26 +0100

    Advent of Code Day 1

---
+++ advent-of-code/day01.hs
@@ -0,0 +1,12 @@
+module Day01 where
+
+solve :: String -> Int
+solve = sum . map mapPar
+
+mapPar :: Char -> Int
+mapPar '(' = 1
+mapPar ')' = -1
+mapPar _   = 0
+
+main :: IO ()
+main = print =<< solve <$> getLine
