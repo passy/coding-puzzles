d40eecb2bd7821eedfd0b3c0605980c909ec39ab
Pascal Hartig <phartig@rdrei.net>
2015-12-27 14:35:07 +0100

    Day 2, second part

--- advent-of-code/day02.hs
+++ advent-of-code/day02.hs
@@ -5,9 +5,10 @@ module Main where
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 
-import Data.List (foldl')
+import Data.List (foldl', sort)
+import System.Environment
 import Control.Applicative
-import Data.Attoparsec.Text
+import Data.Attoparsec.Text hiding (take)
 
 data Dimens = Dimens { length :: Int
                      , width :: Int
@@ -34,5 +35,18 @@ solve contents =
       sumup acc dim = acc + area dim + smallest dim
   in foldl' sumup 0 <$> dimens
 
+solve' :: T.Text -> Either String Int
+solve' contents =
+  let dimens = parseOnly fileParser contents
+      wrap (Dimens l w h) = (*2) $ sum $ take 2 $ sort [l, w, h]
+      bow (Dimens l w h) = product [l, w, h]
+      sumup acc dim = acc + wrap dim + bow dim
+  in foldl' sumup 0 <$> dimens
+
 main :: IO ()
-main = print =<< solve <$> TIO.getContents
+main = do
+  [arg] <- getArgs
+  case arg of
+    "1" -> print =<< solve <$> TIO.getContents
+    "2" -> print =<< solve' <$> TIO.getContents
+    _ -> putStrLn "Run the program with either 1 or 2 as argument."
