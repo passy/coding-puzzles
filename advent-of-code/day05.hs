c4fa392be3bb5971bcfb02bca7b296ff1e559c5d
Pascal Hartig <phartig@rdrei.net>
2015-12-27 22:51:31 +0100

    Avoid explicit recursion

--- advent-of-code/day05.hs
+++ advent-of-code/day05.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE ExplicitForAll #-}
+
 module Main where
 
-import qualified Data.Set as S
-import Data.List (foldl')
+import           Data.List           (foldl', group)
+import qualified Data.Set            as S
 
-import Control.Applicative (liftA2)
+import           Control.Applicative (liftA2)
 
 -- | A lifted ('&&').
 (<&&>) :: Applicative f => f Bool -> f Bool -> f Bool
@@ -13,6 +15,10 @@ import Control.Applicative (liftA2)
 vowels :: S.Set Char
 vowels = S.fromList "aeiou"
 
+pairs :: forall a. [a] -> [[a]]
+pairs (x0:x1:xs) = [x0, x1] : pairs (x1:xs)
+pairs _ = []
+
 badSequences :: S.Set String
 badSequences = S.fromList ["ab", "cd", "pq", "xy"]
 
@@ -20,14 +26,10 @@ hasEnoughVowels :: String -> Bool
 hasEnoughVowels = (>= 3) . length . filter (`S.member` vowels)
 
 hasTwiceInARow :: String -> Bool
-hasTwiceInARow (x0:x1:xs) | x0 == x1 = True
-                          | otherwise = hasTwiceInARow (x1:xs)
-hasTwiceInARow _ = False
+hasTwiceInARow = any ((>= 2) . length) . group . pairs
 
 hasBadSequence :: String -> Bool
-hasBadSequence (x0:x1:xs) | [x0, x1] `S.member` badSequences = True
-                          | otherwise = hasBadSequence (x1:xs)
-hasBadSequence _ = False
+hasBadSequence = any (`S.member` badSequences) . pairs
 
 isNice :: String -> Bool
 isNice = hasEnoughVowels <&&> hasTwiceInARow <&&> (not . hasBadSequence)
