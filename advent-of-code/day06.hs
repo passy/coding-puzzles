962fb0da793f74e078cd20dcb95a2d5a3815f6c6
Pascal Hartig <phartig@rdrei.net>
2015-12-28 20:02:07 +0100

    Parser expanding

--- advent-of-code/day06.hs
+++ advent-of-code/day06.hs
@@ -6,22 +6,23 @@ import Prelude hiding (getContents)
 
 import Text.Megaparsec
 import Text.Megaparsec.Text (Parser ())
-
-import Control.Applicative ((<*), (*>))
+import Control.Arrow ((>>>))
+import Control.Applicative ((<*), (*>), liftA2)
 import Data.Text.IO (getContents)
+import Control.Monad (join)
 
 import qualified Data.Text as T
 import qualified Text.Megaparsec.Lexer as L
 
 type Coord = (Integer, Integer)
 
-data Op = TurnOn Coord Coord | TurnOff Coord Coord | Toggle Coord Coord
+data Op = TurnOn Coord | TurnOff Coord | Toggle Coord
   deriving (Show, Eq)
 
 opsParser :: Parser [Op]
-opsParser = many (opParser <* eol) <* eof
+opsParser = join <$> many (opParser <* eol) <* eof
 
-opParser :: Parser Op
+opParser :: Parser [Op]
 opParser = try (opP "turn off " TurnOff)
        <|> try (opP "turn on " TurnOn)
        <|> opP "toggle " Toggle
@@ -39,14 +40,20 @@ opParser = try (opP "turn off " TurnOff)
       to <- coordTupleP
       return (from, to)
 
-    opP :: String -> (Coord -> Coord -> r) -> Parser r
+    opP :: String -> (Coord -> r) -> Parser [r]
     opP s f = do
       string s
       (from, to) <- coordsP
-      return $ f from to
+      return $ expand f from to
 
--- solve :: T.Text -> Int
-solve = parse opsParser ""
+    expand :: (Coord -> r) -> Coord -> Coord -> [r]
+    expand f (fx, fy) (tx, ty) = [f (x, y) | x <- [fx..tx], y <- [fy..ty]]
+
+solve :: T.Text -> Either ParseError Integer
+solve s = eval <$> parse opsParser "<stdin>" s
+
+eval :: [Op] -> Integer
+eval = undefined
 
 main :: IO ()
 main = print =<< solve <$> getContents
