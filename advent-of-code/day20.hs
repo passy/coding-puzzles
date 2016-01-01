ec9bacf44caf9a0d2e982a8cc232f31a6e940aa6
Pascal Hartig <phartig@rdrei.net>
2016-01-01 23:09:19 +0100

    Replaced my attempt with Peter Seibel's Tweet-sized solution

--- advent-of-code/day20.hs
+++ advent-of-code/day20.hs
@@ -1,20 +1,7 @@
 module Main where
 
-import Data.List (find)
+import Data.List
+import Data.Numbers.Primes
 
-elf :: Int -> [Int]
-elf n = cycle $ n : replicate (n - 1) 0
-
-elfs :: [[Int]]
-elfs = map elf [10, 20..]
-
-presents :: [Int]
-presents = foldr f [0] elfs
-  where
-    f (a:as) bs = a : zipWith (+) as bs
-
-solve :: Int -> Maybe (Integer, Int)
-solve n = find ((>= n) . snd) $ zip [1..] presents
-
-main :: IO ()
-main = print =<< solve <$> readLn
+p = (>= 2900000) . sum . map product . nub . tail . subsequences . primeFactors
+main = print $ find p [1..]
