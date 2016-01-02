2351987b5dc4237ac2755890156cc9b383dca04f
Pascal Hartig <phartig@rdrei.net>
2016-01-02 20:27:41 +0100

    Inorder stuff

--- trees-from-inorder/Main.hs
+++ trees-from-inorder/Main.hs
@@ -1,9 +1,34 @@
+{-# LANGUAGE ExplicitForAll #-}
+
 module Main where
 
 import Control.Monad
+import Control.Monad.Trans.State
+import Data.List
+import Data.Maybe
 
-getTrees :: [Int] -> [Int]
-getTrees _ = []
+data Node a = Node
+  { val :: a
+  , left :: Maybe (Node a)
+  , right :: Maybe (Node a)
+  } deriving (Show, Eq)
+
+getTrees :: [Int] -> [Node Int]
+getTrees inorders = execState (go (fromJust $ splits inorders)) []
+  where
+    go :: Maybe [([Int], Int, [Int])] -> State [Node Int] ()
+    go Nothing = return ()
+    go (Just inputs) = do
+      let ls = go ()
+
+splits :: forall m a. MonadPlus m => [a] -> m [([a], a, [a])]
+splits xs =
+   mapM
+      (\(y, zs0) ->
+         case zs0 of
+            z:zs -> return (y, z, zs)
+            [] -> mzero)
+      (init (zip (inits xs) (tails xs)))
 
 main :: IO ()
 main =
