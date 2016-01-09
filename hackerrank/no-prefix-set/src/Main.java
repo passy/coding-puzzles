53b2a3b81329fb770adf2c062f33fac55eb1ec94
Pascal Hartig <phartig@rdrei.net>
2016-01-09 15:15:10 +0000

    Trie with 3 failures in Java

--- hackerrank/no-prefix-set/src/Main.java
+++ hackerrank/no-prefix-set/src/Main.java
@@ -1,6 +1,59 @@
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(final String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+
+        final int n = scanner.nextInt();
+        final Trie t = new TrieImpl();
+
+        for (int i = 0; i < n; i++) {
+            final String line = scanner.nextLine().trim();
+            if (!t.add(line)) {
+                System.out.printf("BAD SET\n%s", line);
+                return;
+            }
+        }
+
+        System.out.println("GOOD SET");
     }
+}
+
+interface Trie {
+    boolean add(String word);
+}
+
+class TrieImpl implements Trie {
+    private final TrieNode root = new TrieNode();
+
+    @Override
+    public boolean add(final String word) {
+        TrieNode node = root;
+        int index = 0;
+        final int length = word.length();
+        while (index < length) {
+            node.leaf = false;
+            final char c = word.charAt(index);
+            final int i = c - 'a';
+
+            if (node.children[i] == null) {
+                node.children[i] = new TrieNode();
+            } else if (node.children[i].leaf || (index == (length - 1))) {
+                return false;
+            }
+
+            node = node.children[i];
+            index += 1;
+        }
+
+        return true;
+    }
+}
+
+class TrieNode {
+    public static final int RANGE = ('j' - 'a') + 1;
+
+    public boolean leaf = true;
+    public TrieNode[] children = new TrieNode[RANGE];
 }
