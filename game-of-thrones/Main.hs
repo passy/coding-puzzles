01f6888ff2407de9f225f090ea62e45d05ead694
Pascal Hartig <phartig@rdrei.net>
2016-01-12 20:59:29 +0000

    O(n) game-of-thrones

--- game-of-thrones/Main.hs
+++ game-of-thrones/Main.hs
@@ -1,17 +1,20 @@
 module Main where
 
-import Control.Applicative
-import Data.List (permutations)
+-- Grr, Hackerrank, upgrade your GHC.
+import Prelude hiding ((<$>))
+
+import Control.Applicative ((<$>))
+import qualified Data.Map as M
 
 formatBool :: Bool -> String
 formatBool True = "YES"
 formatBool False = "NO"
 
-isPalindrome :: String -> Bool
-isPalindrome s = reverse s == s
+counts :: String -> M.Map Char Int
+counts = M.fromListWith (+) . flip zip (repeat 1)
 
 run :: String -> Bool
-run = any isPalindrome . permutations
+run = (<= 1) . M.size . M.filter odd . counts
 
 main :: IO ()
 main = putStrLn =<< formatBool . run <$> getLine
