7ccf969a6815df24ab637a7b78eb231165557782
Pascal Hartig <phartig@rdrei.net>
2016-01-16 00:15:45 +0000

    Functional approach to utopian-tree

--- hackerrank/utopian-tree/Main.hs
+++ hackerrank/utopian-tree/Main.hs
@@ -2,13 +2,10 @@ module Main where
 
 import Control.Applicative ((<$>))
 import Control.Monad (replicateM_)
+import Data.List (foldl')
 
 cycles :: Int -> Int
-cycles n = go 1 0
-  where
-    go acc c | c == n = acc
-             | odd c = go (acc + 1) (c + 1)
-             | otherwise = go (acc * 2) (c + 1)
+cycles n = foldl' (flip id) 1 $ take n $ concat $ repeat [(* 2), (+ 1)]
 
 main :: IO ()
 main = do
