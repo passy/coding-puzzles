0dbc5d5ccf7b2b424be3c24ae33a57924f69d5c8
Pascal Hartig <phartig@rdrei.net>
2016-01-16 12:35:45 +0000

    even-tree: DFS for calculating height

--- hackerrank/even-tree/main.js
+++ hackerrank/even-tree/main.js
@@ -8,18 +8,49 @@ const run = (lines) => {
   const m = fst[1];
 
   const edges = {};
+  const indeg = {};
 
   for (let i = 1; i <= n; i += 1) {
     edges[i] = [];
+    indeg[i] = 0;
   }
 
   for (let i = 0; i < m; i += 1) {
     let e = inp.shift();
-    edges[e[0]].push(e[1]);
-    edges[e[1]].push(e[0]);
+    const ui = e[0];
+    const vi = e[1];
+    if (ui >= vi) {
+      edges[vi].push(ui);
+      indeg[ui] += 1;
+    } else {
+      edges[ui].push(vi);
+      indeg[vi] += 1;
+    }
   }
 
+  const entries = Object.keys(indeg).filter(i => indeg[i] === 0);
+
+  if (entries.length !== 1) {
+    throw new Error("Illegal tree structure. Either graph or forest.")
+  }
+
+  const depths = calcDepths(entries[0], edges);
+
   console.log(edges);
+  console.log(depths);
+};
+
+const calcDepths = (start, edges) => {
+  const depths = {};
+  Object.keys(edges).forEach(e => { depths[e] = 1; });
+
+  const go = s => {
+    edges[s].forEach(go);
+    depths[s] = edges[s].reduce((b, a) => b + depths[a], 1);
+  };
+
+  go(start);
+  return depths;
 };
 
 const main = () => {
