48413f2f7a67b5adc647b54443b3675d4dedfa59
Pascal Hartig <phartig@rdrei.net>
2016-01-26 20:58:13 +0000

    coin-change: DP solution in Java, failing 3 tests

---
+++ hackerrank/coin-change/src/Solution.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String args[]) {
+        final Scanner scanner = new Scanner(System.in);
+
+        final int n = scanner.nextInt();
+        final int m = scanner.nextInt();
+        final int[] ms = new int[m];
+
+        for (int i = 0; i < m; i++) {
+            ms[i] = scanner.nextInt();
+        }
+
+        System.out.println(changeWays(ms, n));
+    }
+
+    public static int changeWays(int[] coins, int sum) {
+        final int[] cache = new int[sum + 1];
+        cache[0] = 1;
+
+        for (final int coin : coins) {
+            for (int i = coin; i < cache.length; i++) {
+                cache[i] += cache[i - coin];
+            }
+        }
+        return cache[sum];
+    }
+}
