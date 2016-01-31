e87191492fe08ca91c886db3afee73b384410e58
Pascal Hartig <phartig@rdrei.net>
2016-01-31 13:42:29 +0000

    simple-regex: done

--- algorithms/simple-regex/src/Main.java
+++ algorithms/simple-regex/src/Main.java
@@ -4,25 +4,42 @@ public class Main {
         assert matches("A.CD", "ABCD");
         assert !matches("A.BC", "ABC");
         assert !matches("A.BC", "ABCD");
-//        assert matches("A.*BCD", "ABCD");
-//        assert matches("A.*CD", "ABCD");
-//        assert matches("A.*D", "ABCD");
-//        assert !matches("A.*D", "BCD");
+        assert matches("A*BCD", "ABCD");
+        assert matches("A*BCD", "BCD");
+        assert matches("A*BCD", "AAAABCD");
+        assert matches("A.*BCD", "ABCD");
+        assert matches("A.*CD", "ABCD");
+        assert matches("A.*D", "ABCD");
+        assert !matches("A.*D", "BCD");
     }
 
     private static boolean matches(final String pattern, final String string) {
-        for (int i = 0, j = 0; i < pattern.length(); i++) {
-            final char p = pattern.charAt(i);
+        if (pattern.isEmpty()) {
+            return string.isEmpty();
+        } else if (pattern.length() == 1 || pattern.charAt(1) != '*') {
+            if (string.isEmpty()) {
+                return false;
+            }
 
-            if (p == '.') {
-                j++;
-            } else {
-                char s = string.charAt(j++);
+            if (pattern.charAt(0) != '.' && pattern.charAt(0) != string.charAt(0)) {
+                return false;
+            }
 
-                if (s != p) return false;
+            return matches(pattern.substring(1), string.substring(1));
+        } else {
+            if (matches(pattern.substring(2), string)) {
+                return true;
+            }
+
+            for (int i = 0;
+                 i < string.length() &&
+                    (pattern.charAt(0) == '.' || string.charAt(i) == pattern.charAt(0));
+                 i++) {
+                if (matches(pattern.substring(2), string.substring(i + 1))) {
+                    return true;
+                }
             }
         }
-
-        return true;
+        return false;
     }
 }
