08f35099eae659398f4fd181dc10cf4338f2931c
Pascal Hartig <phartig@rdrei.net>
2016-01-31 16:23:29 +0000

    More reversing linked list funz

--- algorithms/reverse-linked-list/src/Main.java
+++ algorithms/reverse-linked-list/src/Main.java
@@ -1,11 +1,10 @@
-import java.util.Optional;
-import java.util.Stack;
+import java.util.*;
 
 public class Main {
 
     static class Node<T> {
         T val;
-        Optional<Main.Node<T>> next;
+        Optional<Node<T>> next;
 
         public Node(T val, Main.Node next) {
             this.val = val;
@@ -27,11 +26,14 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        final Node node = new Node(1, new Node(2, new Node(3, new Node(4, new Node(5)))));
+        final Node<Integer> node = new Node(1, new Node(2, new Node(3, new Node(4, new Node(5)))));
 
         System.out.println(node);
         printReverseRec(node);
         printReverseIter(node);
+
+        System.out.println(reverseRec(Optional.of(node), new LinkedList<Integer>()));
+        System.out.println(reverseIter(Optional.of(node), new LinkedList<Integer>()));
     }
 
     static <T> void printReverseRec(final Node<T> node) {
@@ -54,5 +56,37 @@ public class Main {
         while (!ts.empty()) {
             System.out.println(ts.pop());
         }
+    }
+
+    static <T> List<T> reverseRec(final Optional<Node<T>> node, final List<T> acc) {
+        if (!node.isPresent()) {
+            return acc;
+        }
+
+        final List<T> ts = reverseRec(node.get().next, acc);
+        acc.add(node.get().val);
+
+        return ts;
+    }
+
+    static <T> List<T> reverseIter(final Optional<Node<T>> node, final List<T> acc) {
+        if (!node.isPresent()) {
+            return Collections.emptyList();
+        }
+
+        final Stack<T> ts = new Stack<>();
+
+        Optional<Node<T>> cur = node;
+        do {
+            ts.push(cur.get().val);
+            cur = cur.get().next;
+        } while (cur.isPresent());
+
+        final List l = new LinkedList<>();
+        while (!ts.empty()) {
+            l.add(ts.pop());
+        }
+
+        return l;
     }
 }
