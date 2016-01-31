0f2732fdda370b04d64fe76c57d113ec083d31bc
Pascal Hartig <phartig@rdrei.net>
2016-01-31 20:24:35 +0000

    msearch: java

--- msearch/src/Main.java
+++ msearch/src/Main.java
@@ -12,11 +12,53 @@ public class Main {
                 new char[] { 'r', 'm', 'l' }
         };
 
-        findEntries(matrix, word).stream()
-                .forEach(ints -> System.out.println(Arrays.toString(ints)));
+        final List<int[]> entries = findEntryPoints(matrix, word.substring(1));
+        final boolean[] visited = new boolean[matrix.length * matrix[0].length];
+
+        boolean found = false;
+        for (final int[] entry : entries) {
+            if (walkWord(matrix, word, entry, visited)) {
+                found = true;
+                break;
+            }
+        }
+
+        System.out.println("Found? " + found);
     }
 
-    private static List<int[]> findEntries(final char[][] matrix, final String word) {
+    private static boolean walkWord(final char[][] matrix, final String word, final int[] entry, final boolean[] visited) {
+        if (word.isEmpty()) return true;
+        visited[matrix.length * entry[0] + entry[1]] = true;
+        boolean res = directionsFrom(entry)
+                .stream()
+                .filter(coord -> isValid(matrix, coord))
+                .filter(coord -> !visited[matrix.length * coord[0] + coord[1]])
+                .filter(coord -> matrix[coord[0]][coord[1]] == word.charAt(0))
+                .map(coord -> walkWord(matrix, word.substring(1), coord, visited))
+                .anyMatch(a -> a);
+
+        System.out.printf("w: %s, e: %s, r: %s\n", word, Arrays.toString(entry), res);
+        return res;
+    }
+
+    private static boolean isValid(final char[][] matrix, final int[] coord) {
+        final int n = matrix.length;
+        final int m = matrix[0].length;
+
+        return coord[0] >= 0 && coord[0] < n &&
+                coord[1] >= 0 && coord[1] < m;
+    }
+
+    private static List<int[]> directionsFrom(final int[] entry) {
+        return Arrays.asList(
+                new int[] { entry[0] - 1, entry[1] },
+                new int[] { entry[0], entry[1] - 1 },
+                new int[] { entry[0] + 1, entry[1] },
+                new int[] { entry[0], entry[1] + 1 }
+        );
+    }
+
+    private static List<int[]> findEntryPoints(final char[][] matrix, final String word) {
         final char fst = word.charAt(0);
         final List<int[]> res = new LinkedList<>();
         for (int i = 0; i < matrix.length; i++) {
