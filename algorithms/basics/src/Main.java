666ab42ba5b5f3ef7436034340dc5de652338488
Pascal Hartig <phartig@rdrei.net>
2016-02-03 20:57:30 +0000

    Bigint addition

--- algorithms/basics/src/Main.java
+++ algorithms/basics/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(facIter(10));
+        System.out.println(bigAdd("102", "18"));
     }
 
     public static long fib(int n) {
@@ -35,5 +35,42 @@ public class Main {
         }
 
         return r;
+    }
+
+    public static String bigAdd(String a, String b) {
+        final int max = Math.max(a.length(), b.length());
+        final int[] as = asIntArray(a, max);
+        final int[] bs = asIntArray(b, max);
+
+        int carry = 0;
+        StringBuilder res = new StringBuilder(max);
+        for (int i = 0; i < max; i++) {
+            int tmp = as[i] + bs[i] + carry;
+            if (tmp >= 10) {
+                carry = 1;
+                tmp -= 10;
+            } else {
+                carry = 0;
+            }
+
+            res.append(tmp);
+        }
+
+        return res.reverse().toString();
+    }
+
+    private static int[] asIntArray(final String a, final int l) {
+        final int al = a.length();
+        final int[] as = new int[l];
+
+        for (int i = 0; i < l; i++) {
+            if (i < al) {
+                as[i] = a.charAt(al - 1 - i) - '0';
+            } else {
+                as[i] = 0;
+            }
+        }
+
+        return as;
     }
 }
